976f5ba2e39dea5a7963a986e39f78d586980afb
Chelsea Kent <chelsea.kent@gmail.com>
2018-10-04 08:36:18 -0700

    set up class for game

--- src/components/game.js
+++ src/components/game.js
@@ -2,17 +2,28 @@ import React from 'react';
 
 import Header from './header';
 import GuessSection from './guess-section';
-import GuessCount  from './guess-count';
+import GuessCount from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
-}
+export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentGuess: 0,
+      pastGuesses: [],
+      targetNumber: 0,
+      feedback: 'Make your guess!'
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        <Header />
+        <GuessSection feedback={this.state.feedback} />
+        <GuessCount count={this.state.pastGuesses.length} />
+        <GuessList guesses={this.state.pastGuesses} />
+      </div>
+    );
+  }
+}
