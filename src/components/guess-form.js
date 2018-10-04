3f6b1b2781c218608fed551ab3377f94b6904de8
Chelsea Kent <chelsea.kent@gmail.com>
2018-10-04 11:14:16 -0700

    guess form

--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -2,31 +2,33 @@ import React from 'react';
 
 import './guess-form.css';
 
-export default function GuessForm(props) {
-  return (
-    <form>
-      <input
-        type="text"
-        name="userGuess"
-        id="userGuess"
-        className="text"
-        maxLength="3"
-        autoComplete="off"
-        placeholder="Enter your Guess"
-        required
-      />
-      <input
-        type="submit"
-        id="guessButton"
-        className="button"
-        name="submit"
-        value="Guess"
+export default class GuessForm extends React.Component {
+  render() {
+    return (
+      <form
         onSubmit={e => {
           e.preventDefault();
-          console.log(e.target.value);
-          props.guess(e.target.value);
+          // props.guess(e.input.value);
         }}
-      />
-    </form>
-  );
+      >
+        <input
+          type="text"
+          name="userGuess"
+          id="userGuess"
+          className="text"
+          maxLength="3"
+          autoComplete="off"
+          placeholder="Enter your Guess"
+          required
+        />
+        <input
+          type="submit"
+          id="guessButton"
+          className="button"
+          name="submit"
+          value="Guess"
+        />
+      </form>
+    );
+  }
 }
